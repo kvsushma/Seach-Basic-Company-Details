de949409e413ff0771f22b993806bc21358dccdc
sushma <sushma6oct@gmail.com>
2020-12-16 11:37:53 +0530

    search page ,routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-    root to: "home#index"
+  	devise_for :users
+  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	get '/search' => 'pages#search', :as => 'search_page'
+  	
+	post '/search/:id' , to: 'pages#search'
 end
